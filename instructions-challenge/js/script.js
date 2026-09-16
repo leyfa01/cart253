ff61296d5f00b3ac45f446b4c3edb3ee237702ad
Leyfa <118757352+leyfa01@users.noreply.github.com>
2026-09-16 18:52:04 -0400

    Added a sky, two mountains and the ground

--- instructions-challenge/js/script.js
+++ instructions-challenge/js/script.js
@@ -11,9 +11,42 @@
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
  */
-function setup() {}
+function setup() {
+  createCanvas(400, 300);
+  
+
+}
 
 /**
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
  */
-function draw() {}
+function draw() {
+  background(255, 204, 0);
+  sky();
+  mountainLeft();
+  mountainRight();
+  
+  
+}
+function sky() {
+  push();
+  fill(173, 216, 230);
+  noStroke();
+  rect(0, 0, 400, 150);
+  pop();
+}
+function mountainLeft(){
+  push();
+  fill(150, 90, 0);
+  noStroke();
+  triangle(60, 150, 116, 40, 172, 150);
+  pop();
+}
+function mountainRight(){
+  push();
+  fill(150, 90, 0);
+  noStroke();
+  triangle(228, 150, 284, 40, 340, 150);
+  pop();
+}
+
