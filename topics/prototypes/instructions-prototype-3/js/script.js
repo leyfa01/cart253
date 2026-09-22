09dfd2d43eadb2e3db961433eebbaa19b489f167
Leyfa <118757352+leyfa01@users.noreply.github.com>
2026-09-22 14:27:12 -0400

    Happy robot completed
    changed the two antennas by a central antenna, adjusting the eyebrows, and changing the eye color.

--- topics/prototypes/instructions-prototype-3/js/script.js
+++ topics/prototypes/instructions-prototype-3/js/script.js
@@ -12,7 +12,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-    createCanvas(500,500, WEBGL);
+    createCanvas(600,600, WEBGL);
     
 }
 
@@ -23,6 +23,8 @@ function setup() {
 function draw() {
     background(200);
     sadRobot();
+    happyRobot();
+    angryRobot();
     
 }
 
@@ -72,7 +74,6 @@ function sadRobot(){
     // right side of the mouth
     push();
     translate(50,70,140);
-    
     box(50,50);
     pop();
 
@@ -98,21 +99,88 @@ function sadRobot(){
     box(90,20);
     pop();  
 
-    // left antenna
+    // antenna
     push();
-    translate(-80,-180,0);
+    translate(0,-180,0);
     fill(0,0,0);
-    rotateZ(-0.5);
-    box(40,90,40);
+    box(30,120,30);
     pop();
 
-    // right antenna
+    // 
     push();
-    translate(80,-180,0);
-    fill(0,0,0);
-    rotateZ(0.5);
-    box(40,90,40);
+    fill(100);
+    translate(0,-250,0);
+    sphere(45,45);
     pop();
 }
 
+function happyRobot(){
+    
+    // left eye
+    push();
+    translate(-75,-50,-140);
+    pointLight(200, 200, 0, 0, -900, -900);
+    sphere(40,40,5);
+    pop();
+    
+    // right eye
+    push();
+    translate(75,-50,-140);
+    pointLight(200, 200, 0, 0, -900, -900);
+    sphere(40,40,5);
+    pop();
 
+    // left eye contour
+    push();
+    fill(0,0,0);
+    noStroke();
+    translate(-75,-50,-150);
+    torus(35, 15,)
+    pop();
+
+    // right eye contour
+    push();
+    fill(0,0,0);
+    noStroke();
+    translate(75,-50,-150);
+    torus(35, 15,)
+    pop();
+    
+    // mouth
+    push();
+    translate(0,70,-140);
+    box(50,50);
+    pop();
+
+    // right side of the mouth
+    push();
+    translate(50,70,-140);
+    
+    box(50,50);
+    pop();
+
+    // left side of the mouth
+    push();
+    translate(-50,70,-140);
+    box(50,50);
+    pop();
+
+    // right eyebrow
+    push();
+    translate(80,-130,-150);
+    fill(0,0,0);
+    box(90,20);
+    pop();
+
+    // left eyebrow
+    push();
+    translate(-80,-130,-150);
+    fill(0,0,0);
+    box(90,20);
+    pop();  
+
+}
+
+function angryRobot(){
+
+}
