8f4290b119c0018189b457f53127dcbdfb0606b9
Leyfa <118757352+leyfa01@users.noreply.github.com>
2026-09-22 12:16:26 -0400

    Added comments
    Prototype #2 completed

--- topics/prototypes/instructions-prototype-2/js/script.js
+++ topics/prototypes/instructions-prototype-2/js/script.js
@@ -1,15 +1,15 @@
 /**
- * Title of Project
+ * Geometric Chaos - Intructions Prototype #2
  * Leyna Feknous
  * 
- * An exploration of geometric shapes using different sizes, colors, layers, and rotations
+ * An exploration of geometric with 2D and 3D shapes and using different sizes, colors, layers, and rotations
  * 
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Created a canva and added WEBGL to display 3D shapes
 */
 function setup() {
  createCanvas(500,500, WEBGL);
@@ -17,7 +17,8 @@ function setup() {
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Added a pink background and called different functions 
+ * to display all the shapes
 */
 function draw() {
     background(250,170,180);
@@ -28,43 +29,60 @@ function draw() {
     donut();
 }
 
+/**
+ * Created a 2D circle and 3D sphere
+ */
 function circles(){
+    // 2D cicle
     push();
+    // double the weight and the height of the ellipse 
     ellipseMode(RADIUS);
     noStroke();
     fill(70,130,120);
     ellipse(50, 50, 100, 100);
     pop();
 
+    // 3D sphere
     push();
     noStroke();
+    // Move the sphere (x,y)
     translate(110,70);
     fill(200,120,150);
     sphere();
     pop();
 }
-
+/**
+ * Created a 2D rectangle and 3D box
+ */
 function rectangles(){
+    // 2d rectangle
     push();
     rectMode(RADIUS);
     fill(100, 250, 300);
     translate(-100,-100);
     noStroke();
-    rotateZ(2);
+    // rotate the rectangle 
+    rotate(2);
     rect(0, 0, 80,80)
     pop();
 
+    // 3D box
     push();
     strokeWeight(0.2);
     colorMode(RGB, 100);
     fill(70, 195, 10);
     translate(-100, -100, 0);
     box();
+    // let the user move around and view the 3D shapes from different angles
+    // the value changes the sensitivity of the movement
     orbitControl(2, 2, 2);
     pop();
     
 }
 
+/**
+ * Created a curved line
+ */
 function funLine(){
     push();
     noFill();
@@ -73,9 +91,13 @@ function funLine(){
     bezier(100, -200, -90, -300, 190, 190, -150, 180);
     pop();
 }
+/**
+ * Created a 3D sphere and changed its shape to make it look like a diamond
+ */
 function diamond(){
     push();
     strokeWeight(0.3);
+    // color range 0-100
     colorMode(RGB, 100);
     fill(100, 100, 60);
     translate(120, -120, 10);
@@ -83,6 +105,7 @@ function diamond(){
     sphere(50,15,2);
     pop();
 }
+// Created a 3D torus, changed its color, moved its position, and rotated it
 function donut(){
     push();
     colorMode(RGB, 200);
